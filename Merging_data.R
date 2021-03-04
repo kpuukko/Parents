ccf10e2150ad6f23c0a29635ddd9e2777824630e
Puukko <kpuukko@ad.helsinki.fi>
2021-03-04 12:49:15 +0200

    Updating merged data

--- Merging_data.R
+++ Merging_data.R
@@ -27,12 +27,10 @@ library(MASS)
 
 # Import data to R #
 
-setwd('C:/LocalData/kpuukko/Parents2021')
+setwd('C:/LocalData/kpuukko/Parents')
 
-sfulldata <- read.spss("C:/LocalData/kpuukko/Parents2021/6_8luokka_oppilasdata.sav", use.value.labels=FALSE, to.data.frame=TRUE)
-
+sfulldata <- read.spss("C:/LocalData/kpuukko/Parents/6_8luokka_oppilasdata.sav", use.value.labels=FALSE, to.data.frame=TRUE)
 sfulldata
-
 
 # Oppilasdata #
 
@@ -48,7 +46,7 @@ bdig19, cdig1, cdig2, cdig3, cdig7, cdig8, cdig9, cdig10, cdig11, cdig13, cdig16
 conlea1, conlea2, conlea3, conlea4, conlea5, conlea6, negadigi1, negadigi2, negadigi3, negadigi4,
 negadigi5, Ofina, MGender)
 
-glimpse(od) #2707 vastausta 77 muuttujaa
+glimpse(od) #2707 vastausta, 77 muuttujaa
 str(od)
 
 # Oppilasdata Sum scores
@@ -56,14 +54,15 @@ od$oeiu <- rowMeans(od[,c("negadigi1", "negadigi2", "negadigi3", "negadigi4", "n
 
 # Vanhempien data #
 
-glimpse(d) #1392 vastausta 93 rivi‰
+glimpse(d) #1392 vastausta, 93 muuttujaa
 summary(d)
 str(d)
 head(d)
 
 # Merging datafiles #
 gdata <- left_join(d, od, by= "ID") #kaikki vanhempien rivit oli niiden lapsista dataa tai ei
-ydata <- inner_join(d, od, by = "ID") #m‰ts‰‰ vain vanhemmat joiden lapsista on dataa ja lapset joiden vanhemmista on dataa
+ydata <- inner_join(d, od, by = "ID") #m?ts?? vain vanhemmat joiden lapsista on dataa ja lapset joiden vanhemmista on dataa
+joindata <- merge(x = d, y = od[ , c("ID", "MGender")], by = "ID", all.x=TRUE) #yhdist√§√§ pelk√§n sukupuolen vanhempien datasta
 
 glimpse(gdata)
 str(gdata)
@@ -71,17 +70,19 @@ str(gdata)
 glimpse(ydata)
 str(ydata)
 
-ydata <- as.data.frame(ydata)
-gdata <- as.data.frame(gdata)
+# Duplicated check #
+uusi_df <- gdata %>% 
+  dplyr::select(ena1:exc_c)
 
-#Number of children 
-ydata <- ydata %>% #jatkuva
-  mutate(cnumb = as.factor(cnumb))
+head(d)
 
-gdata <- gdata %>% #jatkuva
-  mutate(cnumb = as.factor(cnumb))
+uusi_df[duplicated(uusi_df),]
+check <- gdata %>% filter(ID == 52116 | ID == 22221)
 
-str(ydata)
+gdata <- gdata %>% distinct(ena1, ena2, ena3, ena4, ena5, res1, res2, res3, mon1, mon2, mon3, 
+                            tech1, tech2, rev1, skill1, skill2, skill3, skill4, skill5, skill6,
+                            skill7, skill8, skill9, skill10, skill11, esm1, esm2, esm3, egm1, egm2,
+                            egm3, role, Edu, q6, q8, ID, .keep_all = T) #listaa muuttujat, jolla saa oikeaan rivim√§√§r√§√§n ID ja eda
 
 # Save Mplus data #
 prepareMplusData(ydata, "merged_mplus2.dat")
